cd7595d4e20a5348683a6998d804b77440e14369
Ezequiel <ezequiel1de1oliveira@gmail.com>
2021-04-12 16:58:30 -0300

    mapping all gpio ports
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- src/gpio.c
+++ src/gpio.c
@@ -4,18 +4,31 @@ void bcm2835_setup() {
     if(!bcm2835_init())
         exit(0);
 
+    // Outputs
     bcm2835_gpio_fsel(LAMPADA_1, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(LAMPADA_2, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(LAMPADA_3, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(LAMPADA_4, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(AR_1, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_fsel(AR_2, BCM2835_GPIO_FSEL_OUTP);
+
+    // Inputs
     bcm2835_gpio_fsel(SENSOR_PRESENCA_1, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_set_pud(SENSOR_PRESENCA_1, BCM2835_GPIO_PUD_UP);
     bcm2835_gpio_fsel(SENSOR_PRESENCA_2, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_set_pud(SENSOR_PRESENCA_2, BCM2835_GPIO_PUD_UP);
     bcm2835_gpio_fsel(SENSOR_ABERTURA_1, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_set_pud(SENSOR_ABERTURA_1, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_fsel(SENSOR_ABERTURA_2, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(SENSOR_ABERTURA_2, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_fsel(SENSOR_ABERTURA_3, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(SENSOR_ABERTURA_3, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_fsel(SENSOR_ABERTURA_4, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(SENSOR_ABERTURA_4, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_fsel(SENSOR_ABERTURA_5, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(SENSOR_ABERTURA_5, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_fsel(SENSOR_ABERTURA_6, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(SENSOR_ABERTURA_6, BCM2835_GPIO_PUD_UP);    
 }
 
 void ligar_lampada(int comando) {
