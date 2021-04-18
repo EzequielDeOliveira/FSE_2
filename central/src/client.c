fd7a3528afe2c7974ea69ddb058521e50cb05f13
Ezequiel <ezequiel1de1oliveira@gmail.com>
2021-04-18 04:50:44 -0300

    try simple sockets

--- central/src/client.c
+++ central/src/client.c
@@ -9,51 +9,56 @@
 #define LEN 16
 
 char *IP_Server = "192.168.0.52";
-char *Port_Server = "10115";
+unsigned short Port_Server = 10115;
 
 void getbme280(int command)
 {
     int clienteSocket;
     struct sockaddr_in servidorAddr;
+    unsigned short servidorPorta;
+    char *IP_Servidor;
+    char *mensagem;
+    char buffer[16];
     unsigned int tamanhoMensagem;
-    unsigned int totalBytesRecebidos;
-    unsigned int bytesRecebidos;
-    char buffer[LEN];
-    char response[LEN];
 
-    tamanhoMensagem = snprintf(buffer, 6, "%d", command);
+    int bytesRecebidos;
+    int totalBytesRecebidos;
+
+    IP_Servidor = IP_Server;
+    servidorPorta = Port_Server;
+    mensagem = "0";
 
     if ((clienteSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         printf("Erro no socket()");
 
-        if(send(clienteSocket, buffer, tamanhoMensagem, 0) != tamanhoMensagem)
-        		printf("Erro no envio: numero de bytes enviados diferente do esperado\n");
-
     memset(&servidorAddr, 0, sizeof(servidorAddr));
     servidorAddr.sin_family = AF_INET;
-    servidorAddr.sin_addr.s_addr = inet_addr(IP_Server);
-    servidorAddr.sin_port = htons(Port_Server);
+    servidorAddr.sin_addr.s_addr = inet_addr(IP_Servidor);
+    servidorAddr.sin_port = htons(servidorPorta);
 
-    if(connect(clienteSocket, (struct sockaddr *) &servidorAddr, 
-							sizeof(servidorAddr)) < 0)
+    if (connect(clienteSocket, (struct sockaddr *)&servidorAddr,
+                sizeof(servidorAddr)) < 0)
         printf("Erro no connect()\n");
 
-    
-    if(send(clienteSocket, buffer, tamanhoMensagem, 0) != tamanhoMensagem)
-        		printf("Erro no envio: numero de bytes enviados diferente do esperado\n");
+    tamanhoMensagem = strlen(mensagem);
 
-    bytesRecebidos = recv(clienteSocket, response, LEN-1, 0);
-    if(bytesRecebidos < 0)
+    if (send(clienteSocket, mensagem, tamanhoMensagem, 0) != tamanhoMensagem)
+        printf("Erro no envio: numero de bytes enviados diferente do esperado\n");
+
+    totalBytesRecebidos = recv(clienteSocket, buffer, 16 - 1, 0);
+
+    if (totalBytesRecebidos < 1)
+    {
         printf("Não recebeu o total de bytes enviados\n");
+    }
 
-    response[15] = '\0';
-
+    buffer[totalBytesRecebidos] = '\0';
 
     float temperature;
     float humidity;
     int command_received;
-    sscanf(response, "%d %lf %lf", &command_received, &temperature, &humidity);
+    sscanf(buffer, "%d %f %f", &command_received, &temperature, &humidity);
+    printf("%s\n%d %f %f\n", buffer, command_received, temperature, humidity);
 
     close(clienteSocket);
-    printf("%d %lf %lf\n", &command_received, &temperature, &humidity);
 }
--- distributed/src/server.c
+++ distributed/src/server.c
@@ -5,64 +5,53 @@
 #include <string.h>
 #include <unistd.h>
 
-#define serverPort 10115
-
-void TrataClientTCP(int socketClient)
-{
-    char request[16];
-    char response[16];
+void TrataClientTCP(int socketClient) {
+    char buffer[16];
     int tamanhoRecebido;
 
-    if ((tamanhoRecebido = recv(socketClient, request, 16, 0)) < 0)
+    if((tamanhoRecebido = recv(socketClient, buffer, 16, 0)) < 0)
         printf("Erro no recv()\n");
 
-    while (tamanhoRecebido > 0)
-    {
-
-        request[16] = '\0';
-        printf("%d", atoi(request));
-
-/* 
-        sscanf(request, "%d %lf %lf", &command_received, &temperature, &humidity);
-
-        if(send(socketClient, response,tamanhoRecebido, 0) != tamanhoRecebido)
+    while(tamanhoRecebido > 0) {
+        if(send(socketClient, buffer,tamanhoRecebido, 0) != tamanhoRecebido)
             printf("Erro no envio - send()\n");
 
-        if((tamanhoRecebido = recv(socketClient, request, 16, 0)) < 0)
+        if((tamanhoRecebido = recv(socketClient, buffer, 16, 0)) < 0)
             printf("Erro no recv()\n");
-   */  }
-}
+    }   
+}  
 
-void receive_messages(int argc, char *argv[])
-{
+void receive_messages(int argc, char *argv[]) {
     int servidorSocket;
     int socketCliente;
     struct sockaddr_in servidorAddr;
     struct sockaddr_in clienteAddr;
+    unsigned short servidorPorta;
     unsigned int clienteLength;
 
-    if ((servidorSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    servidorPorta = 10115;
+
+    if((servidorSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         printf("falha no socker do Servidor\n");
 
     memset(&servidorAddr, 0, sizeof(servidorAddr));
     servidorAddr.sin_family = AF_INET;
     servidorAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servidorAddr.sin_port = htons(serverPort);
+    servidorAddr.sin_port = htons(servidorPorta);
 
-    if (bind(servidorSocket, (struct sockaddr *)&servidorAddr, sizeof(servidorAddr)) < 0)
+    if(bind(servidorSocket, (struct sockaddr *) &servidorAddr, sizeof(servidorAddr)) < 0)
         printf("Falha no Bind\n");
 
-    if (listen(servidorSocket, 10) < 0)
+    if(listen(servidorSocket, 10) < 0)
         printf("Falha no Listen\n");
 
-    while (1)
-    {
+    while(1) {
         clienteLength = sizeof(clienteAddr);
-        if ((socketCliente = accept(
-                 servidorSocket,
-                 (struct sockaddr *)&clienteAddr,
-                 &clienteLength)) < 0)
-            printf("Falha no Accept\n");
+        if((socketCliente = accept(
+            servidorSocket,
+            (struct sockaddr *) &clienteAddr,
+            &clienteLength)) < 0)
+                printf("Falha no Accept\n");
 
         printf("Conexão do Cliente %s\n", inet_ntoa(clienteAddr.sin_addr));
 
